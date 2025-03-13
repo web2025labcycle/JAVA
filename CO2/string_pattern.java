6f30756ebfdd1fac172e2c478a292a281f00c139
AFSSANA A <naafa.afaan@gmail.com>
2025-03-13 13:47:17 +0530

    Update string_pattern.java

--- CO2/string_pattern.java
+++ CO2/string_pattern.java
@@ -1,8 +1,8 @@
-// If package name is needed
+
 package cycle2; 
 
 
-public class string_pattern {  // Ensure class name matches filename (StringPattern.java)
+public class string_pattern {  
     public static void main(String[] args) {
         String text = "Java is an object oriented programming language";
         String newText = text.replaceAll("Java", "python");
